ae151dc4ff118ddeae3297d3952b70eee98aa1f2
Thisas <thisas@thisas.dev>
2023-06-11 13:06:45 +0400

    Added sub questions !!!

--- pdf_magic.py
+++ pdf_magic.py
@@ -1,4 +1,5 @@
 import pdfplumber
+import re
 
 def extract_left_margin_words(pdf_path):
     with pdfplumber.open(pdf_path) as pdf:
@@ -20,10 +21,31 @@ def extract_left_margin_words(pdf_path):
                     })
                     
         return left_margin_words
+    
+def extract_sub_questions(pdf_path):
+    with pdfplumber.open(pdf_path) as pdf:
+        sub_questions = []
+        
+        for page_number, page in enumerate(pdf.pages, 1):
+            page_text = page.extract_text()
+            page_words = page.extract_words()
+            
+            for word in page_words:
+                text = word['text']
+                # Use regex to check for sub-question pattern (brackets with a single alphabet)
+                if re.match(r'\([a-zA-Z]\)', text):
+                    sub_questions.append({
+                        'sub_question': text,
+                        'line_number': word['top'],
+                        'page_number': page_number
+                    })
+        
+        return sub_questions
 
 # Usage example
 pdf_path = 'demo.pdf'
 left_margin_words = extract_left_margin_words(pdf_path)
+sub_questions = extract_sub_questions(pdf_path)
 
 # Print the extracted numeric words with line number and page number
 for word_info in left_margin_words:
@@ -32,3 +54,10 @@ for word_info in left_margin_words:
         line_number = word_info['line_number']
         page_number = word_info['page_number']
         print(f"Word: {word}, Line: {line_number}, Page: {page_number}")
+
+# Print the extracted sub-questions with line number and page number
+for sub_question_info in sub_questions:
+    sub_question = sub_question_info['sub_question']
+    line_number = sub_question_info['line_number']
+    page_number = sub_question_info['page_number']
+    print(f"Sub-question: {sub_question}, Line: {line_number}, Page: {page_number}")
